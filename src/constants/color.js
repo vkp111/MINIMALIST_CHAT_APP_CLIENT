73b64bb123de98c48aabd15b6d0761ebe20fd87e
Vijayant Pandey <v.pandey0212@gmail.com>
2025-01-07 18:43:47 +0530

    Backend Completed

---
+++ src/constants/color.js
@@ -0,0 +1,10 @@
+export const orange = "#285F6B";
+export const orangeLight = "#4c4c4c";
+
+export const grayColor = "rgba(247,247,247,1)";
+export const lightBlue = "#2694ab";
+export const matBlack = "#010001";
+export const bgGradient = "linear-gradient(#285F6B, #133336)";
+
+export const purple = "#367D8A";
+export const purpleLight = "rgba(75,12,192,0.2)";
