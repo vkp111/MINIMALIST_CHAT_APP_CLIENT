6409d7d3489b22d16d562e90d2a425b03674d457
Vijayant Pandey <119390705+vkp111@users.noreply.github.com>
2025-01-07 19:45:21 +0530

    Update socket.jsx
    Add the render url here too

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,13 +1,20 @@
 import { createContext, useMemo, useContext } from "react";
 import io from "socket.io-client";
-import { server } from "./constants/config";
+// import { server } from "./constants/config";
 
 const SocketContext = createContext();
 
 const getSocket = () => useContext(SocketContext);
 
-const SocketProvider = ({ children }) => {
-  const socket = useMemo(() => io(server, { withCredentials: true }), []);
+cconst SocketProvider = ({ children }) => {
+  const socket = useMemo(
+    () =>
+      io("https://minimalist-chat-app-server.onrender.com", {
+        withCredentials: true,
+        transports: ["websocket"], // Force WebSocket transport
+      }),
+    []
+  );
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
